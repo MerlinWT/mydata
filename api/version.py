44efb341a33d7a70138b3aa4681fbd02d1f7047a
KovalenkoAV <merlinwt@gmail.com>
2020-09-25 16:38:29 +0300

    use python3

--- api/version.py
+++ api/version.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-print "Content-Type: application/json\n"
-print "{\"current\": \"0.0.1\"}"
+print("Content-Type: application/json\n")
+print("{\"current\": \"0.0.1\"}", end="")
